7950a29e6ef55cf6e608690f35fded35983b0442
Julia Pereira da Costa <juliapcosta97@gmail.com>
2024-01-07 20:00:01 -0300

    First commit Spring init

---
+++ src/main/java/br/com/fiap/techchallengepayments/TechChallengePaymentsApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.techchallengepayments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechChallengePaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechChallengePaymentsApplication.class, args);
+	}
+
+}
